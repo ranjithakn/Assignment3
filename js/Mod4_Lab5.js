a4ea5143f676f1534beba1bf8923ac9bcf3020ae
Ranjitha Kn <ranju.kn@gmail.com>
2023-02-28 13:26:20 -0800

    MOD4_Lab5

--- js/Mod4_Lab5.js
+++ js/Mod4_Lab5.js
@@ -2,7 +2,8 @@
 
 /*Validate Repeat Entry in Lab 5(Extra Credit 5 points)
 
-There’s a line of code in lab 5 that asks the user if they want to repeat their entry.The code resembled the image outlined below.
+There’s a line of code in lab 5 that asks the user if they want to repeat their entry.The code resembled the 
+image outlined below.
 
 Your job in this part of the assignment is to validate that entry.If it’s anything other than “y” or “n”,
 the prompt should simply reappear until the user enters a valid entry.*/
@@ -11,6 +12,7 @@ let miles;
 let gallons;
 let mpg;
 let again = 'y';
+let flag = 0;
 
 do {
     miles = parseFloat(prompt('Enter miles driven'));
@@ -21,8 +23,15 @@ do {
     } else {
         alert('One or both entries are invalid.');
     }
-    again = prompt('Run application again? (y or n)', 'y');
-    if (again !== 'y' || again !== 'n')
-        again = prompt('Run application again? (y or n)', 'y');
+
+    //again = prompt('Run application again? (y or n)', 'y');
+    again = 'n';
+    do {
+        if (again !== 'y')
+            again = prompt('Run application again? (y or n)', 'y');
+        if (again === 'n')
+            break;
+    } while (again !=='y' );
+        
 } while (again === 'y');
 document.writeln('Application has exited.');
